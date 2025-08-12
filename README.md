28e9ec4b1e6c873b42242fc722d207e5668318bf
Elgrinch001 <santyelmejor105@gmail.com>
2025-08-11 21:56:13 -0500

    Descuento

--- README.md
+++ README.md
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-## Getting Started
-
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
-=======
 # TiendaCafePOO
->>>>>>> e732f2f515e3fadb9b04d363699fc456af825cb7
+
+Este proyecto contiene las clases Cafe y TiendaCafe desarrolladas en Java como parte de la actividad de POO.
--- src/TiendaCafe.java
+++ src/TiendaCafe.java
@@ -1,8 +1,9 @@
 public class TiendaCafe {
     public static void main(String[] args) throws Exception {
-        Cafe cafe1 = new Cafe("Café de Nariño", "Nariño", 32000f, 10f);
-        Cafe cafe2 = new Cafe("Café de Antioquia", "Antioquia", 28000f, 8f);
-        Cafe cafe3 = new Cafe("Café del Huila", "Huila", 30000f, 5f);
+
+        Cafe cafe1 = new Cafe("Café de Nariño", "Nariño", 3000.0, 9.0);
+        Cafe cafe2 = new Cafe("Café de Antioquia", "Antioquia", 28000.0, 8.0);
+        Cafe cafe3 = new Cafe("Café del Huila", "Huila", 30000.0, 5.0);
 
         cafe1.mostrarInformacion();
         cafe2.mostrarInformacion();
@@ -13,9 +14,15 @@ public class TiendaCafe {
         System.out.println("Precio total de " + cafe3.nombre + ": $" + cafe3.calcularPrecioTotal());
         System.out.println("-----------------------------");
 
-        cafe2.actualizarCantidad(12f);
+        cafe2.actualizarCantidad(12.0);
+        System.out.println("Inventario actualizado✏️");
         System.out.println("Cantidad actualizada de " + cafe2.nombre + ":");
         cafe2.mostrarInformacion();
-    }
-    }
 
+        // Método para aplicar descuento al café
+
+        System.out.println("Aplicando descuento del 10% al primer café...");
+        cafe1.aplicarDescuento(10.0);
+        cafe1.mostrarInformacion();
+    }
+}
